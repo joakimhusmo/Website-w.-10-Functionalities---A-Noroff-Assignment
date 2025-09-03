bf2e903be3662f37419f48325b6d36eab5616d70
Joakim Husmo <joakimhusmo@outlook.com>
2025-09-03 12:34:34 +0200

    Add sharedstate.js module to manage global variable across files

--- modules/sharedstate.js
+++ modules/sharedstate.js
@@ -1,2 +1,11 @@
-// this is a file to share values across files
+// this is a file to share values across files. Functions that share state is routed through this file. 
 
+export let global; 
+
+export function setGlobal(value) {
+    global = value; 
+}
+
+export function getGlobal() {
+    return global; 
+}

ac03071698e46a398af8925e9d57a184ce0604db
Joakim Husmo <joakimhusmo@outlook.com>
2025-09-04 13:44:09 +0200

    Edit JS file to correct source code errors

--- modules/q9_infiniteLoop.js
+++ modules/q9_infiniteLoop.js
@@ -1,11 +1,22 @@
 // In Function infiniteLoop(), create an Infinite loop which asks the user in a prompt to write “STOP”. The loop should end when the correct input is given. If the user provides any other data, ask them again.
 
-
+/*
 export function infiniteLoop() {
     while(true) {
         const res = prompt("Write STOP to exit");
         if(res === "STOP") {
             continue;   
-        }
+        } 
     }
 }
+*/
+
+export function infiniteLoop() {
+    while(true) {
+        const res = prompt("Write STOP to exit");
+        if(res === "STOP") {
+            break;  // Replace `continue` with `break`                                  
+        } 
+    }
+}
+

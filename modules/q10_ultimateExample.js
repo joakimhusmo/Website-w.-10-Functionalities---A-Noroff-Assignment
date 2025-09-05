7c7d88224e2367b09be562bfd51f1270f405dd0e
Joakim Husmo <joakimhusmo@outlook.com>
2025-09-05 11:27:33 +0200

    Edit JS file to correct the source code errors

--- modules/q10_ultimateExample.js
+++ modules/q10_ultimateExample.js
@@ -1,14 +1,52 @@
-// Function ultimateExample() asks the user to provide the number (n). Generate an image of size n times n and show it in the console. Based on the Math.random() method, generate the image consisting of these characters:
+// Function ultimateExample() 
+// asks the user to provide the number (n). 
+// Generate an image of size n times n and show it in the console. 
+// Based on the Math.random() method, generate the image consisting of these characters:
 // “ “
 // “|”
 // “_”
 // “X”
 // “.”
-// After generating an image of n=10, something similar to this is shown in the console:
-
-
+// After generating an image of n=10, something similar to this is shown in the console: [image].
 
 export function ultimateExample() { 
+    const n = parseInt(prompt("Please provide the length of the side of the square"))
+    if (isNaN(n) || n < 0 && n > 50) {  
+        alert("Wrong data");
+        return;                        // return if input has failed
+    }    
+    for(let i=0; i<n; i++) {           // Replace i-- with i++
+        let line = "";                 // loop empty string
+        
+        for (let j=0; j<n; j++) {      // increment loop 
+            const number = Math.floor(Math.random() * 5); // Replace `.round` w. `.floor` & `*4` w. `*5` 
+            switch(number) {
+                case 0:                 // Replace `default` w. `case 0`
+                    line += " ";
+                    break;
+                case 1:
+                    line += ".";
+                    break;
+                case 2:
+                    line += "X";
+                    break;
+                case 3:
+                    line += "|";
+                    break;
+                case 4:
+                    line += "_";
+                    break;
+            }
+        }
+        console.log(line);
+        
+    }
+}
+
+/*
+Original code: 
+
+function ultimateExample() {
     const n = parseInt(prompt("Please provide the length of the side of the square"))
     if (isNaN(n) || n < 0 && n > 50) {  
         alert("Wrong data");
@@ -38,4 +76,4 @@ export function ultimateExample() {
         }
         console.log(line);
     }
-}
+*/
